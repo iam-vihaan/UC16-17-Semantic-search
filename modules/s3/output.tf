91a3b1eb599e09d631ab348f4954225cdf8dccb4
iam-vihaan <asathyavihaan@gmail.com>
2025-06-27 11:23:02 +0530

    Update output.tf

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -1,9 +1,9 @@
 output "bucket_name" {
   description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.this.id
+  value       = aws_s3_bucket.semeantic-search.id
 }
 
 output "bucket_arn" {
   description = "ARN of the S3 bucket"
-  value       = aws_s3_bucket.this.arn
+  value       = aws_s3_bucket.semeantic-search.arn
 }
