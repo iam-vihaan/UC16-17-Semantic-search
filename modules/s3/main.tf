81cbf742c0002be26a0c0d6c39df8818e0df9a81
iam-vihaan <asathyavihaan@gmail.com>
2025-06-27 11:31:13 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,9 +1,4 @@
-resource "aws_s3_bucket" "UC16-semeantic-search" {
+resource "aws_s3_bucket" "UC1617-semeantic-search" {
   bucket = var.bucket_name
   tags   = var.tags
-}
-
-
-resource "random_id" "bucket_id" {
-  byte_length = 4
 }
