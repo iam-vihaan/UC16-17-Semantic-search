47f093a1912316049c442e4b3dd0b6f2b8089b09
iam-vihaan <asathyavihaan@gmail.com>
2025-06-27 11:35:28 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,4 +1,8 @@
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "UC1617-semeantic-search" {
-  bucket = var.bucket_name
+  bucket = "${var.bucket_name}-${random_id.bucket_id.hex}"
   tags   = var.tags
 }
