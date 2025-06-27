d9d45399406a6dc926d678ea5f477395f40de587
iam-vihaan <asathyavihaan@gmail.com>
2025-06-27 12:28:58 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -6,3 +6,21 @@ resource "aws_s3_bucket" "UC1617-semeantic-search" {
   bucket = "${var.bucket_name}-${random_id.bucket_id.hex}"
   tags   = var.tags
 }
+
+resource "aws_s3_bucket_policy" "lambda_access" {
+  bucket = aws_s3_bucket.UC1617-semeantic-search.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::784733659029:role/lambda_exec_role"
+        },
+        Action = "s3:GetObject",
+        Resource = "arn:aws:s3:::lambda-code-bucket-ddd/*"
+      }
+    ]
+  })
+}
